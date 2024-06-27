d335a8049c476f377e52a662781383f383a0fd75
ochamu <171050612+ochamu@users.noreply.github.com>
2024-06-28 08:57:24 +0900

    #24 Station13 完成

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if(check.checked){
+    text.style.backgroundColor = "red";
+  }
+  else{
+    text.style.backgroundColor = "white";
+  }
 }
